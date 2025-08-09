9dfa622d7d602eab227a1e900302fc9ce3c5f34b
Marcel Owhonda <owhondamarcelprince@gmail.com>
2025-08-08 23:06:02 -0700

    updated code

--- Web-App/App3/app.py
+++ Web-App/App3/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from App 3"
+@app.route('/')
+def home():
+    return "Hello, App3 | Ingress"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
